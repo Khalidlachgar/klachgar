579532d34f580711e65865c0ed8bff5c31e1d249
khalidlachgar <khalidlachgarkhalid@gmail.com>
2024-09-24 18:58:17 +0200

    firstex

---
+++ ex03.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+
+ int main ()
+{
+       float yards,km;
+       printf ("veuillez entrer la transforme en yards ");
+       scanf ("%2.f", &yards);
+       yards = km * 1093.61;
+       printf ("en yards est :%2.f\n",yards );
+    return 0;
+}
