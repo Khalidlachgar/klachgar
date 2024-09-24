7cb83b3f216912e3575d4ea71797e874fddfd746
khalidlachgar <khalidlachgarkhalid@gmail.com>
2024-09-24 19:11:01 +0200

    forstex

--- ex04.c
+++ ex04.c
@@ -3,7 +3,7 @@
  int main ()
 {
        float ms,kmh;
-       printf ("la vitesse en km/h ");
+       printf ("veuillez entrer la vitesse en kmh :");
        scanf ("%2.f", &kmh);
        ms = kmh * 0.27778;
        printf ("la transforme en ms est :%f\n",ms );
