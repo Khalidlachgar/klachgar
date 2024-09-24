a43b0494fb374ef271103ffc33ca1e618132ddf3
khalidlachgar <khalidlachgarkhalid@gmail.com>
2024-09-24 18:45:04 +0200

    firstex

---
+++ ex 01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+     char prenome[12];
+     char nom [20];
+     char sexe [40];
+     char email [90];
+     int age;
+
+
+     printf ("veuillez le prenome :");
+     scanf ("%s,&le_prenome :");
+     printf ("veuillez le nom :");
+     scanf ("%s,&le_nom :");
+     printf ("veuillez le sexe :");
+     scanf ("%s,&le_sexe :");
+     printf ("veuillez l'email :");
+     scanf ("%s,&l'email :");
+     printf ("veuillez l'age :");
+     scanf ("%d,&l'age :");
+     return 0;
