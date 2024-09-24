1642b41e84e0237d91c0d2b4fc197e9e11011d90
khalidlachgar <khalidlachgarkhalid@gmail.com>
2024-09-24 18:53:35 +0200

    firstex

---
+++ ex02 (2).c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+ int main ()
+{
+       float k,C;
+       printf ("veuille la température en C ");
+       scanf ("%2.f",&C);
+       k = C + 273.15;
+ return 0;
+}
